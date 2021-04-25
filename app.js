268dba222d6ffa80be7a069bfd96a17113e16e90
geor1812 <george.ursachi7@gmail.com>
2021-04-25 12:45:55 +0200

    Added the basics of frontpage, header, footer

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
+app.use(express.static(__dirname + "/public"));
+
+const fs = require("fs");
+
+const header = fs.readFileSync(__dirname + "/public/header/header.html", "utf-8");
+const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
+
+const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
+
+app.get("/", (req, res) => {
+    res.send(header + frontpage + footer);
+});
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
     if (error) {
