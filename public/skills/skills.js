fd7b6cc4b7df542d8ea0bb8025854d4bb0e0561d
geor1812 <george.ursachi7@gmail.com>
2021-04-27 17:57:21 +0200

    Dynamically adding skills to the page

--- public/skills/skills.js
+++ public/skills/skills.js
@@ -1,11 +1,51 @@
-
+const colorDictionary = {
+    language: "bg-info",
+    fandl: "bg-success",
+    service: "bg-warning",
+    industry: "bg-danger"
+};
 
 (async function getSkills() {
     try {
-        const response = await fetch("/api/projects");
+        const response = await fetch("/api/skills");
         const result = await response.json();
 
+        const languageDiv = document.getElementById("language");
 
+        result.data.map(skill => {
+            const rowDiv = document.createElement("div");
+            rowDiv.className = "row";
+
+            const cardDiv = document.createElement("div");
+            cardDiv.className = "card bg-dark w-75 my-2";
+
+            const cardBodyDiv = document.createElement("div");
+            cardBodyDiv.className = "card-body";
+
+            const skillTitle = document.createElement("h3");
+            skillTitle.className = "card-title";
+            skillTitle.innerText = skill.title;
+
+            const skillDescription = document.createElement("p");
+            skillDescription.className = "card-text";
+            skillDescription.innerText = skill.description;
+
+            const progressBackground = document.createElement("div");
+            progressBackground.className = "progress bg-secondary";
+
+            const progressBar = document.createElement("div");
+            progressBar.className = "progress-bar progress-bar-striped progress-bar-animated";
+            progressBar.classList.add(colorDictionary[skill.type]);
+            progressBar.setAttribute("style", `width: ${skill.progress * 20}%`);
+
+            languageDiv.appendChild(rowDiv);
+            rowDiv.appendChild(cardDiv);
+            cardDiv.appendChild(cardBodyDiv);
+            cardBodyDiv.appendChild(skillTitle);
+            cardBodyDiv.appendChild(skillDescription);
+            cardBodyDiv.appendChild(progressBackground);
+            progressBackground.appendChild(progressBar);
+        });
 
     } catch (error) {
         console.log(error);
